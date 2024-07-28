bdaa45f42cb6a7d17105f68df522bdb86b00b011
akoval25 <88685936+akoval25@users.noreply.github.com>
2024-07-28 14:48:33 +0300

    mobile fix

--- crm/src/Pagination.js
+++ crm/src/Pagination.js
@@ -1,27 +1,3 @@
-
-// import React from 'react';
-
-
-// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-//   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
-
-//   return (
-//     <div className="pagination">
-//       {pages.map((page) => (
-//         <button
-//           key={page}
-//           className={page === currentPage ? 'active' : ''}
-//           onClick={() => onPageChange(page)}
-//         >
-//           {page}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Pagination;
-
 import React from 'react';
 import chevronR from './img/chevronr.png';
 import chevronL from './img/chevronl.png';
